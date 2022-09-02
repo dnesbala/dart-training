b1dd7b23ae882ed6b35af80921b88578e4e1a9ee
Dinesh Bala <baladnes@gmail.com>
2022-09-02 19:45:49 +0545

    Added conditions in dart

---
+++ conditions.dart
@@ -0,0 +1,41 @@
+// if(condition) {
+// run, if true
+// }
+
+void main() {
+  // int age = 15;
+
+  // if (age > 18) {
+  //   print("You are eligible to vote");
+  // } else {
+  //   print("You are not eligible to vote");
+  // }
+
+  int day = 1;
+
+  // if (day == 1) {
+  //   print("Sunday");
+  // } else if (day == 2) {
+  //   print("Monday");
+  // } else if (day == 3) {
+  //   print("Tuesday");
+  // } else {
+  //   print("Invalid day");
+  // }
+
+  switch (day) {
+    case 1:
+      print("Sunday");
+      break;
+    case 2:
+      print("Monday");
+      break;
+    case 3:
+      print("Tuesday");
+      break;
+    default:
+      print("Invalid day");
+  }
+
+  print("outside of switch");
+}
