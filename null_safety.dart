7cb260c0652ff462c7831614fa8e563f4506f95c
Dinesh Bala <baladnes@gmail.com>
2022-09-04 20:59:30 +0545

    Added null safety in dart

---
+++ null_safety.dart
@@ -0,0 +1,28 @@
+// int num2 = null;  // Non-nullable - can't be null
+
+void main() {
+  // int? num = null; // Nullable - can be null
+  // if (num == null) {
+  //   print("Please Enter value");
+  // } else {
+  //   print("Value is not null");
+  // }
+
+  // String? name = null;
+
+  // String? name2 = name;
+  // String name2 = name!;
+  // String name2 = name ?? "Null value";
+  // String? name2;
+  // name2 ??= "Null value";
+
+  // print(name2);
+
+  // String? name3;
+  // print(name3!.toUpperCase());
+
+  // late String name;
+
+  // name = "John";
+  // print(name);
+}
